47c9b2d1d460835e4c8c9b3ad13627162473cd50
Jason <jason_skillman@outlook.com>
2020-03-24 21:10:44 -0400

    Created smelter class

---
+++ Assets/Scripts/Smelter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Smelter : MonoBehaviour, IInteractable {
+
+    public CinemachineVirtualCamera cmCamera;
+    public GameObject hud;
+
+    private bool interactState;
+
+
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    [ContextMenu("Interact")]
+    public void OnInteract() {
+        interactState = !interactState;
+        if(interactState) {
+            ShowHUD();
+            cmCamera.Priority = 100;
+        } else {
+            HideHUD();
+            cmCamera.Priority = 0;
+        }
+    }
+
+    public void ShowHUD() {
+        hud.SetActive(true);
+    }
+
+    public void HideHUD() {
+        hud.SetActive(false);
+    }
+
+}
