cd78181a3725822665d659389f047948d93dc82c
Jason <jason_skillman@outlook.com>
2020-03-27 20:46:50 -0400

    Created basic Pickup script

---
+++ Assets/Scripts/Pickups/Pickup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour, IPickupable {
+
+    [Header("Resource")]
+    public Resource itemResource;
+    public Resource ItemResource {
+        get {
+            return itemResource;
+        }
+        set {
+            itemResource = value;
+        }
+    }
+
+    public bool HasResource {
+        get {
+            return (ItemResource != null);
+        }
+    }
+
+    [Range(1, 99)]
+    public int amount = 1;
+
+
+    void Start() {
+        ItemResource = Instantiate(itemResource);
+    }
+
+    void Update() {
+
+    }
+
+    public void OnPickup() {
+        
+    }
+
+}
---
+++ Assets/Scripts/ScriptableObjects/Resource.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Resource", menuName = "Scriptable Objects/Resource", order = 1)]
+public class Resource : ScriptableObject {
+
+    public string title;
+    public string description;
+
+    [Range(1, 99)]
+    public int amount = 1;
+
+}
