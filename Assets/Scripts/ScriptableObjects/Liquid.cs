d7f8ffc83d042c20ce0f482ef8fd97520737a7e8
Jason <jason_skillman@outlook.com>
2020-03-27 20:51:58 -0400

    Added liquid as a scriptable object

---
+++ Assets/Scripts/ScriptableObjects/Liquid.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Liquid", menuName = "Scriptable Objects/Liquid", order = 2)]
+public class Liquid : ScriptableObject {
+
+    public string title;
+    public Color color;
+    public bool flamable = false;
+
+}
