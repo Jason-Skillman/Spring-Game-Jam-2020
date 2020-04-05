4c4bf3e9ae7eff80cdfb9d7368abeffbafa1e120
Jason <jason_skillman@outlook.com>
2020-04-05 16:40:21 -0400

    Working on rotating the machine

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,10 +11,13 @@ public class GameManager : MonoBehaviour {
 	public GameObject prefabHoverPos;
 	public GameObject prefabHoverNeg;
 
+	[Header("Debug")]
+	public GameObject selectedBuild;
+
 	private Camera cameraMain;
 	private Ray ray;
 	private Vector3 hoverPoint;
-	private GameObject gameObjectHover;
+	private GameObject selectedBuildInstance;
 
 	public enum GameMode {
 		PlayMode,
@@ -28,10 +31,16 @@ public class GameManager : MonoBehaviour {
 	}
 
 	private void Start() {
-		if(prefabHoverPos != null)
+		if(prefabHoverPos)
 			prefabHoverPos = Instantiate(prefabHoverPos);
-		if(prefabHoverNeg != null)
+		if(prefabHoverNeg)
 			prefabHoverNeg = Instantiate(prefabHoverNeg);
+		
+		//Todo: remove later
+		if(selectedBuild != null)
+			selectedBuildInstance = Instantiate(selectedBuild);
+		
+		
 	}
 
 	private void Update() {
@@ -50,6 +59,8 @@ public class GameManager : MonoBehaviour {
 	private void HandleInput() {
 		if(Input.GetKeyDown(KeyCode.Tab)) {
 			ToggleGameMode();
+		} else if(Input.GetKeyDown(KeyCode.R)) {
+			RotateBuild();
 		}
 	}
 
@@ -68,6 +79,11 @@ public class GameManager : MonoBehaviour {
 				interactable.OnInteract();
 			}
 		}
+		
+		//Turn off edit mode gameObjects
+		if(prefabHoverPos) prefabHoverPos.SetActive(false);
+		if(prefabHoverNeg) prefabHoverNeg.SetActive(false);
+		if(selectedBuildInstance) selectedBuildInstance.SetActive(false);
 	}
 
 	private void OnEditMode() {
@@ -94,15 +110,29 @@ public class GameManager : MonoBehaviour {
 			break;
 		}
 				
-		//Show the hover prefab
-		prefabHoverPos.SetActive(true);
+		//Show the edit mode game objects
+		if(prefabHoverPos) prefabHoverPos.SetActive(true);
+		if(selectedBuildInstance) selectedBuildInstance.SetActive(true);
+		
+		//Move the hover objects to the hover point
 		prefabHoverPos.transform.position = hoverPoint;
+		prefabHoverNeg.transform.position = hoverPoint;
+		selectedBuildInstance.transform.position = hoverPoint;
 	}
 
 	[ContextMenu("Toggle Game Mode")]
 	public void ToggleGameMode() {
 		if(gameMode == GameMode.PlayMode) gameMode = GameMode.EditMode;
 		else gameMode = GameMode.PlayMode;
+	}
+
+	public void RotateBuild() {
+		
+		Vector3 vector = selectedBuildInstance.transform.forward * 90;
+
+
+		selectedBuildInstance.transform.rotation =
+			Quaternion.RotateTowards(selectedBuildInstance.transform.rotation, Quaternion.Euler(vector), 90.0f);
 	}
 
 	private void OnDrawGizmos() {
