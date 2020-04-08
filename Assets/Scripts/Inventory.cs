0bf48d4dc13662c7551ee1d5cfb66139deae1f8b
Joel <fallendemongod@gmail.com>
2020-04-08 15:56:50 -0400

    Inserted Inventory System(WIP)

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory
+{
+    private List<Resource> itemList;
+
+    public Inventory()
+    {
+        itemList = new List<Resource>();
+        Debug.Log("Inventory Count:" + itemList.Count);
+    }
+
+
+    public void AddItem(Resource resource)
+    {
+        itemList.Add(resource);
+    }
+
+    public List<Resource> GetItemList()
+    {
+        return itemList;
+    }
+}
---
+++ Assets/Scripts/Test Scripts/TestPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayer : MonoBehaviour
+{    public Inventory inventory;
+
+    [SerializeField]
+    private UI_Inventory uiInventory;
+    [SerializeField]
+    public Resource[] testResources;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        inventory = new Inventory();
+        uiInventory.SetInventory(inventory);
+
+       
+    }
+
+    void Start()
+    {
+        for (int x = 0; x < testResources.Length; x++)
+            inventory.AddItem(testResources[x]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
