87b956bbbded6f38bccdedb37f5243fe0979271e
Jason <jason_skillman@outlook.com>
2020-04-18 15:42:12 -0400

    Added a simple drill

---
+++ Assets/Scripts/Machines/Drill.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drill : MonoBehaviour {
+
+	public Resource resourceToMine;
+	public int amount = 1;
+	public GameObject outputPos;
+	public GameObject prefabTransport;
+
+	private Coroutine coroutineMineTimer;
+
+
+	void Start() {
+		coroutineMineTimer = StartCoroutine(MineTimer());
+	}
+
+	private IEnumerator MineTimer() {
+		while(true) {
+			yield return new WaitForSeconds(1);
+
+			GameObject obj = Instantiate(prefabTransport);
+			obj.transform.position = outputPos.transform.position;
+		
+			Transport transport = obj.GetComponent<Transport>();
+			transport.Resource = Instantiate(resourceToMine);
+			transport.Resource.amount = amount;
+		}
+	}
+}
